97c72e87be6cd754c013a62f759c9ba4aa1694ca
jordanbar1 <jordan.barhoumeh@students.mq.edu.au>
2024-04-10 20:36:37 +1000

    Commented out local run

--- app.py
+++ app.py
@@ -106,8 +106,8 @@ def service_books(church_id):
 
 
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    # app.run(host='192.168.5.129', port=5000, debug=True)
-    app.run(host='0.0.0.0', port=5000, debug = True)
+# if __name__ == '__main__':
+#     with app.app_context():
+#         db.create_all()
+#     # app.run(host='192.168.5.129', port=5000, debug=True)
+#     app.run(host='0.0.0.0', port=5000, debug = True)
