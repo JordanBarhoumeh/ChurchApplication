fcfb6d5f5d65172415665ccc59c8ebdf771628b2
jordanbar1 <jordan.barhoumeh@students.mq.edu.au>
2024-04-12 22:32:09 +1000

    new hamburg

--- static/script.js
+++ static/script.js
@@ -75,3 +75,13 @@ function hideInstallPrompt() {
 }
 
 
+/* -------------------- NEW BURGER MENU ANIMATION -------------------- */
+
+const menu_btn = document.querySelector('.hamburger');
+const mobile_menu = document.querySelector('.mobile-nav');
+
+menu_btn.addEventListener('click', function () {
+    menu_btn.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
+});
+
