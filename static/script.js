9d54d82e0b04e607342502ac697a702e22b5d3ea
jordanbar1 <jordan.barhoumeh@students.mq.edu.au>
2024-04-12 23:32:11 +1000

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,40 +1,6 @@
 /* -------------------- NAV ANIMATION -------------------- */
 
-// Highlight the active link based on the current page
-function highlightActiveLink() {
-    var links = document.getElementsByClassName('nav-link');
-    var currentPage = window.location.pathname;
-    for (var i = 0; i < links.length; i++) {
-        if (links[i].href.includes(currentPage)) {
-            links[i].classList.add('active');
-        }
-    }
-}
 
-// Call this function when the menu is opened
-function openNav() {
-    document.getElementById("slide-out-menu").style.width = "100%";
-    highlightActiveLink(); // Ensure this function is defined or remove this line if not used
-    document.getElementById("burger-stack").style.display = "none"; // Hides the burger icon
-}
-
-// Call this function when the menu is closed
-function closeNav() {
-    document.getElementById("slide-out-menu").style.width = "0";
-    document.getElementById("burger-stack").style.display = "block"; // Shows the burger icon again
-}
-
-document.getElementById("burger-stack").onclick = openNav;
-
-document.addEventListener("DOMContentLoaded", resetButtonStyles);
-window.addEventListener("focus", resetButtonStyles);
-
-function resetButtonStyles() {
-  var buttons = document.querySelectorAll('.move-button');
-  buttons.forEach(function(button) {
-    button.style.backgroundColor = 'var(--navy-accent)'; // Reset to navy color
-  });
-}
 
 
 /* -------------------- EVENT POP UP ANIMATION -------------------- */
