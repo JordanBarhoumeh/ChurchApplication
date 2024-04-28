93ca71f20f0d0ff52c74251ba6e0c642ff8606d5
jordanbar1 <jordan.barhoumeh@students.mq.edu.au>
2024-04-28 21:19:17 +1000

    sw file change

--- static/sw.js
+++ static/sw.js
@@ -1,17 +1,24 @@
-self.addEventListener('fetch', event => {
-  if (event.request.url.includes('/church_main')) {
-    // Network first strategy for specific routes
-    event.respondWith(
-      fetch(event.request).catch(() => {
-        return caches.match(event.request);
-      })
-    );
-  } else {
-    // Cache first strategy for other requests
-    event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request);
-      })
-    );
-  }
-});
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    // Perform action on click, like opening a URL or handling specific data
+    event.waitUntil(clients.openWindow('/some-url'));
+  });
+  
+  self.addEventListener('fetch', event => {
+    if (event.request.url.includes('/church_main')) {
+      // Network first strategy for specific routes
+      event.respondWith(
+        fetch(event.request).catch(() => {
+          return caches.match(event.request);
+        })
+      );
+    } else {
+      // Cache first strategy for other requests
+      event.respondWith(
+        caches.match(event.request).then(response => {
+          return response || fetch(event.request);
+        })
+      );
+    }
+  });
+  
