a685450c4f83e69982c9fe2b772a4567a2d091ec
jordanbar1 <jordan.barhoumeh@students.mq.edu.au>
2024-08-16 17:51:08 +1000

    remove notifications

--- app.py
+++ app.py
@@ -170,9 +170,3 @@ def home():
     return render_template('welcome.html')
 
 
-
-# if __name__ == '__main__':
-#     with app.app_context():
-#         db.create_all()
-#     # app.run(host='192.168.5.129', port=5000, debug=True)
-#     app.run(host='0.0.0.0', port=5000, debug = True)
